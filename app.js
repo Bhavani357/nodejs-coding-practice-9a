fd08f407d1bf221eb30aaf7355b998c3cb27bf23
Bhavani357 <113431655+Bhavani357@users.noreply.github.com>
2023-04-08 19:44:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,9 +52,67 @@ app.post("/register", async (request, response) => {
         `;
     await db.run(createUser);
     response.status(200);
-    response.send("User Created Successfully");
+    response.send("User created successfully");
   } else {
     response.status(400);
     response.send("User already exists");
   }
 });
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `
+    SELECT * FROM user WHERE username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      response.status(200);
+      response.send("Login success!");
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.put("/change-password", async (request, response) => {
+  const { username, oldPassword, newPassword } = request.body;
+  const selectedPassword = `
+    SELECT * FROM user WHERE username = '${username}';`;
+  const dbUser = await db.get(selectedPassword);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("User not registered");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(
+      oldPassword,
+      dbUser.password
+    );
+    if (isPasswordMatched === true) {
+      if (newPassword.length < 5) {
+        response.status(400);
+        response.send("Password is too short");
+      } else {
+        const encryptedPassword = await bcrypt.hash(newPassword, 10);
+        const updatePasswordQuery = `
+                UPDATE 
+                user 
+                SET 
+                password = '${encryptedPassword}'
+                WHERE 
+                username = '${username}';`;
+        const dbUserUpdate = await db.run(updatePasswordQuery);
+        response.status(200);
+        response.send("Password updated");
+      }
+    } else {
+      response.status(400);
+      response.send("Invalid current password");
+    }
+  }
+});
+
